9d1aaa9229745b2e2834b2312cc5b44a2f5cade5
Mark Sapiro <mark@msapiro.net>
2024-07-19 18:50:58 -0700

    Make default bounce_matching_headers be documentaion only.

--- src/mailman/styles/base.py
+++ src/mailman/styles/base.py
@@ -250,11 +250,16 @@ class Moderation:
         mlist.max_message_size = 40               # KB
         mlist.require_explicit_destination = True
         mlist.bounce_matching_headers = """
-# Lines that *start* with a '#' are comments.
-to: friend@public.com
-message-id: relay.comanche.denmark.eu
-from: list@listme.com
-from: .*@uplinkpro.com
+# This is a deprecated setting from Mailman 2.1. Use header_matches instead.
+# Here's an example. Blank lines and lines that *start* with a '#' are ignored.
+#to: friend@public.com
+#message-id: relay.comanche.denmark.eu
+#from: list@listme.com
+#from: .*@uplinkpro.com
+# If the above 4 lines were uncommented, this would cause any message with a
+# To: header matching the pattern 'friend@public.com' or similarly any message
+# with a Message-ID: or From: header matching the respective patterns to be
+# rejected.
 """
         mlist.header_matches = []
         mlist.administrivia = True
