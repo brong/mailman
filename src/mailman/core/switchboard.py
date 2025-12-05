333feb6cc0b55f686cc121d73e5811b8a140c02b
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2025-12-06 00:44:37 +0900

    perf: SwitchBoard: Use os.fdatasync() instead of os.fsync()
    SwitchBoard does not depend on file metadata for robustness.

--- src/mailman/core/switchboard.py
+++ src/mailman/core/switchboard.py
@@ -137,7 +137,7 @@ class Switchboard:
             fp.write(msgsave)
             pickle.dump(data, fp, protocol)
             fp.flush()
-            os.fsync(fp.fileno())
+            os.fdatasync(fp.fileno())
         os.rename(tmpfile, filename)
         return filebase
 
@@ -246,7 +246,7 @@ class Switchboard:
                     pickle.dump(data, fp, protocol)
                     fp.truncate()
                     fp.flush()
-                    os.fsync(fp.fileno())
+                    os.fdatasync(fp.fileno())
                     if data['_bak_count'] >= MAX_BAK_COUNT:
                         elog.error('.bak file max count, preserving file: %s',
                                    filebase)
